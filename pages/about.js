ae270fa3e22141ba4768dbac6ef913871a4e7f95
Shiyu Wang <shiyu3169@gmail.com>
2020-12-23 11:07:56 -0800

    router and style

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div style={style}>
+      <h1>About</h1>
+    </div>
+  )
+}
+
+const style = {
+  fontSize: "20px",
+  color: "blue"
+}
+
+export default About
